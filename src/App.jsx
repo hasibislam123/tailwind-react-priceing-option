6d2a3678adbae9a1ea09e85712cd2e45ea209c2f
Hasib Islam <hasib64dj@gmail.com>
2025-09-27 22:14:54 +0600

    added the new code

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { } from 'react'
+
+import './App.css'
+import DaisyNave from './component/DaisyNave/DaisyNave'
+import Navbar from './component/Navbar/Navbar'
+
+function App() {
+
+
+  return (
+    <>
+      <header>
+        <DaisyNave></DaisyNave>
+        <Navbar></Navbar>
+
+      </header>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Link from './link';
+
+const Navbar = () => {
+    const navData = [
+        { id: 1, name: "Home", path: "/" },
+        { id: 2, name: "About", path: "/about" },
+        { id: 3, name: "Services", path: "/services" },
+        { id: 4, name: "Blog", path: "/blog" },
+        { id: 5, name: "Contact", path: "/contact" },
+    ];
+    return (
+        <nav >
+
+            <ul className='flex'>
+                {
+                    navData.map(route => <Link key={route.id} route={route} ></Link>)
+                }
+            </ul>
+
+            {/* <ul className='flex'>
+              {navData.map(route => < li className='ml-10' >
+              <a href={route.path}>{route.name}</a>
+              </li>)}
+            </ul> */}
+            {/* <ul className='flex '>
+                < li className='ml-10' ><a href="">Home</a></li>
+                < li className='ml-10' ><a href="/about">About</a></li>
+                < li className='ml-10' ><a href="/blog">Blog</a></li>
+            </ul> */}
+        </nav>
+    );
+};
+
+export default Navbar;
